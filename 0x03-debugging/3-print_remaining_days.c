fcff8908950a53aa7849673e224c0e43ee87a93a
diegolopezq95 <927@holbertonschool.com>
2019-06-25 19:36:45 +0000

    C - debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 != 0))
 	{
-		if (month >= 2 && day >= 60)
+		if (month >= 3 && day > 60)
 		{
 			day++;
 		}
@@ -24,11 +24,11 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 2 && day == 60)
+		if ((month > 12) || (month <= 0) || (day > 29) || (day < 0))
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
 		}
-		else
+		else 
 		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
