7579333a97186aeafdb5060c2fe2236d0c6dee34
diegolopezq95 <927@holbertonschool.com>
2019-06-25 23:53:27 +0000

    C - functions, nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,20 +24,18 @@ void print_times_table(int n)
 					{
 						_putchar((k / 100) + '0');
 						_putchar((k / 10) % 10 + '0');
-						_putchar((k % 10) + '0');
 					}
 					else if (k < 100 && k > 9)
 					{
 						_putchar(' ');
 						_putchar((k / 10) + '0');
-						_putchar((k % 10) + '0');
 					}
 					else
 					{
 						_putchar(' ');
 						_putchar(' ');
-						_putchar((k % 10) + '0');
 					}
+					_putchar((k % 10) + '0');
 					if (l != n)
 						_putchar(',');
 					continue;
