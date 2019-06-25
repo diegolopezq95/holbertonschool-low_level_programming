172d1ef58530145fa7530a8d77ef252cecce0a2b
diegolopezq95 <927@holbertonschool.com>
2019-06-25 23:44:41 +0000

    C - functions, nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,23 +1,52 @@
 #include "holberton.h"
 
 /**
- * print_times_times_table - Entry point
- * Description: prints the n times table, starting with 0.
- * Return: Always 0
+ * print_times_table - Entry point
+ * @n: prints the n times table, starting with 0
+ * Description: prints the n times table
  */
 void print_times_table(int n)
 
 {
-	int a, b, c;
+	int m, l, k;
 
-	if (n > 15 || n < 0)
+	if (n >= 0 && n < 15)
 	{
-		for (a = 0; a <= n; a++)
+		for (m = 0; m <= n; m++)
 		{
-			for (b = 0; b <= n; b++)
+			for (l = 0; l <= n; l++)
 			{
-				c = a * b;
-				if (c > 99)
+				k = m * l;
+				if (l != 0)
 				{
-					_putchar((c / 10) + '0');
-					_putchar((c % 10) + '0');
+					_putchar(' ');
+					if (k > 99)
+					{
+						_putchar((k / 100) + '0');
+						_putchar((k / 10) % 10 + '0');
+						_putchar((k % 10) + '0');
+					}
+					else if (k < 100 && k > 9)
+					{
+						_putchar(' ');
+						_putchar((k / 10) + '0');
+						_putchar((k % 10) + '0');
+					}
+					else
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar((k % 10) + '0');
+					}
+					if (l != n)
+						_putchar(',');
+					continue;
+				}
+				_putchar(l + '0');
+				if (n != 0)
+					_putchar(',');
+			}
+			_putchar('\n');
+		}
+	}
+}
