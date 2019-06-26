fadb7e667484dbffd15db481813c5aa03550a660
diegolopezq95 <927@holbertonschool.com>
2019-06-26 21:27:17 +0000

    C - functions, nested loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,31 +1,47 @@
-#include "holberton.h"
 #include <stdio.h>
-
 /**
  * main - Entry point
- * Description: prints the first 50 Fibonacci numbers, starting with 1 and 2
+ * Description: prints the first 98 Fibonacci numbers, starting with 1 and 2
  * Return: Always 0
  */
 int main(void)
 
 {
 	int n;
-	unsigned long int m = 1;
-	unsigned long int l = 2;
-	unsigned long int k;
-	unsigned long int sum = 0;
+	unsigned long int m, l, k, mx, my, lx, ly, x, y, z;
 
+	m = 1;
+	l = 2;
 	if (m == 1 && l == 2)
-	{
-		printf("%ld, %ld", m, l);
-	}
-	for (n = 0; n <= 98; n++)
+		printf("%lu, %lu", m, l);
+	for (n = 0; n <= 88; n++)
 	{
 		k = m + l;
 		m = l;
 		l = k;
-		printf(", %ld", k);
+		printf(", %lu", k);
 	}
-	printf("%ld\n", sum);
+	mx = m / 1000000000000000000;
+	my = m % 1000000000000000000;
+	lx = l / 1000000000000000000;
+	ly = l % 1000000000000000000;
+
+	for (n = 89; n < 96; n++)
+	{
+		y = my + ly;
+		z = y / 1000000000000000000;
+		y = y % 1000000000000000000;
+		x = mx + lx + z;
+		my = ly;
+		mx = lx;
+		ly = y;
+		lx = x;
+		printf(", %lu%lu", x, y);
+		if (n == 95)
+		{
+			printf("\n");
+		}
+	}
 	return (0);
 }
+
