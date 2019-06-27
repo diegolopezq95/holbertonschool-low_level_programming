35c2f878381ba33c95a55e86e2adabca70e25c01
diegolopezq95 <927@holbertonschool.com>
2019-06-27 18:18:17 +0000

    C - more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,27 +1,26 @@
-#include "holberton.h"
 #include <stdio.h>
 
 /**
- * fizz_buzz - prints the numbers from 1 to 100
+ * main - prints the numbers from 1 to 100
  * Return: Always 0.
  */
-int fizz_buzz(void)
+int main(void)
 {
 	int m;
 
 	for (m = 1; m <= 100; m++)
 	{
-		if ((m % 3) == 0)
+		if (m % 15 == 0)
 		{
-			printf("Fizz");
+			printf("FizzBuzz");
 		}
-		else if ((m % 5) == 0)
+		else if (m % 5 == 0)
 		{
 			printf("Buzz");
 		}
-		else if ((m % 15) == 0)
+		else if (m % 3 == 0)
 		{
-			printf("FizzBuzz");
+			printf("Fizz");
 		}
 		else
 		{
