0c6272786d3b350a0074b26a8cd477f4b7040f51
diegolopezq95 <927@holbertonschool.com>
2019-07-04 18:55:35 +0000

    Pointers, arrays & strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * *cap_string - changes all lowercase letters of a string to uppercase.
+ * @s: array of integer
+ * Return: Always 0.
+ */
+char *cap_string(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		if (s[0] >='a' && s[0] <= 'z')
+		{
+			s[0] = (s[0] - 32);
+		}
+		if (s[count - 1] == ' ' && s[count] >= 'a' && s[count] <= 'z')
+		{
+			s[count] = (s[count] - 32);
+		}
+		if (s[count - 1] == '.' && s[count] >= 'a' && s[count] <= 'z')
+                {
+                        s[count] = (s[count] - 32);
+                }
+		if (s[count - 1] == '\t' && s[count] >= 'a' && s[count] <= 'z')
+                {
+                        s[count] = (s[count] - 32);
+                }
+		if (s[count - 1] == '\n' && s[count] >= 'a' && s[count] <= 'z')
+                {
+                        s[count] = (s[count] - 32);
+                }
+		count++;
+	}
+	return (s);
+}
