cd87a05112170ef2c3221bb774e53aab17592ec8
diegolopezq95 <927@holbertonschool.com>
2019-07-05 22:26:27 +0000

    Pointers, arrays & strings

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -11,46 +11,33 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-        int count;
-	int length;
-        int temp;
-        int temp2;
-	int first;
-	int second;
+	int count, length, tmp, temp2, first, second;
 
 	count = 0;
 	length = 0;
-        while (n1[count] != '\0')
+
+	while (n1[count] != '\0')
 	{
-                count++;
+		count++;
 	}
-        while (n2[length] != '\0')
+	while (n2[length] != '\0')
 	{
-                length++;
+		length++;
 	}
-        while (count != 0 && length != 0 && size_r != 0)
+	while (count != 0 && length != 0 && size_r != 0)
 	{
 		if (n1[0] == '-')
-		{
 			first = (n1[count] - '0');
-		}
 		else
-		{
 			first = (n1[count] - '0') * -1;
-		}
 		if (n2[0] == '-')
-		{
 			second = (n2[count] - '0');
-		}
 		else
-		{
 			second = (n2[count] - '0') * -1;
-		}
 		temp = first + second + temp2;
 		if (temp > 9)
 		{
 			temp2 = temp / 10;
-			
 			temp = temp % 10;
 		}
 		r[size_r--] = temp;
@@ -59,11 +46,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		length--;
 	}
 	if (r[0] == '0')
-	{
 		return (r + 1);
-	}
 	else
-	{
-		return (r);	
-	}
+		return (r);
 }
