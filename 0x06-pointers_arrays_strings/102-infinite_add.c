7d29e6c195125f5f64aaa95e61ba9859d924607d
diegolopezq95 <927@holbertonschool.com>
2019-07-05 22:20:42 +0000

    Pointers, arrays & strings

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * *infinite_add - function that adds two numbers.
@@ -10,21 +11,59 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	count = 0;
-	while (s[count] != '\0')
-	{
-		length = 0;
-		while (alf[length] != '\0')
-		{
-			if (s[count] == alf[length])
-			{
-				s[count] = rot[length];
+        int count;
+	int length;
+        int temp;
+        int temp2;
+	int first;
+	int second;
 
-				break;
-			}
-			length++;
-		}
-		count++;
+	count = 0;
+	length = 0;
+        while (n1[count] != '\0')
+	{
+                count++;
 	}
-	return (s);
+        while (n2[length] != '\0')
+	{
+                length++;
+	}
+        while (count != 0 && length != 0 && size_r != 0)
+	{
+		if (n1[0] == '-')
+		{
+			first = (n1[count] - '0');
+		}
+		else
+		{
+			first = (n1[count] - '0') * -1;
+		}
+		if (n2[0] == '-')
+		{
+			second = (n2[count] - '0');
+		}
+		else
+		{
+			second = (n2[count] - '0') * -1;
+		}
+		temp = first + second + temp2;
+		if (temp > 9)
+		{
+			temp2 = temp / 10;
+			
+			temp = temp % 10;
+		}
+		r[size_r--] = temp;
+		printf("%d\n", r[size_r + 1]);
+		count--;
+		length--;
+	}
+	if (r[0] == '0')
+	{
+		return (r + 1);
+	}
+	else
+	{
+		return (r);	
+	}
 }
