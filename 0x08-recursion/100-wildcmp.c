f3cd53a4d4385c59741c82b71279e0fdfc6149d8
diegolopezq95 <927@holbertonschool.com>
2019-07-10 22:13:47 +0000

    Recursion

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -20,7 +20,7 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	}
-	if (*s2 == '*' && (*(s2 + 1) || *(s1 + 1)))
+	if (*s2 == '*' && ((s2 + 1) != '\0' || (s1 + 1) != '\0'))
 	{
 		return (1);
 	}
