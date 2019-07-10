d845bb3200a92070aaef50dad89278779a9ec37e
diegolopezq95 <927@holbertonschool.com>
2019-07-10 18:24:02 +0000

    Recursion

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 
 /**
- * _is_length - calculates the length.
+ * is_length - calculates the length.
  * @s: string
  * Return: .
  */
-int _is_length(char *s)
+int is_length(char *s)
 {
 	if (*s != '\0')
-        {
-		return (1 + _is_length(s + 1));
+	{
+		return (1 + is_length(s + 1));
 	}
 	else
 	{
@@ -17,34 +17,34 @@ int _is_length(char *s)
 	}
 }
 /**
- * _is_palindrome_function - calculates if the function is palindrome.
+ * is_palindrome_function - calculates if the function is palindrome.
  * @s: string
  * @m: length1
  * @n: lengthrev
  * Return: .
  */
-int _is_palindrome_function(char *s, int m, int n)
+int is_palindrome_function(char *s, int m, int n)
 {
 	if (m == n)
 	{
 		return (1);
-	} 
+	}
 	if (m > n || n < m)
 	{
 		return (1);
-	} 
+	}
 	if (s[m] == s[n])
 	{
-		return (_is_palindrome_function(s, m + 1, n - 1));
+		return (is_palindrome_function(s, m + 1, n - 1));
 	}
 	return (0);
 }
 /**
- * is_prime_number - returns 1 if a string is a palindrome and 0 if not.
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
  * @s: string
  * Return: .
  */
 int is_palindrome(char *s)
 {
-	return (_is_palindrome_function(s, 0, _is_length(s) - 1));
+	return (is_palindrome_function(s, 0, is_length(s) - 1));
 }
