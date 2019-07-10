fd2821ef422535aad21cd2157044cfb051503a2d
diegolopezq95 <927@holbertonschool.com>
2019-07-10 21:46:37 +0000

    Recursion

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,38 +1,28 @@
 #include "holberton.h"
 
 /**
- * wildcmp_function - compares two strings with wildcard.
+ * wildcmp - compares two strings with wildcard.
  * @s1: string 1
- * @s2: string 2
+ * @s2: string 2 with wildcards
  * Return: .
  */
-int wildcmp_function(char *s1, char *s2)
+int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
-        {
-                return (1);
-        }
-	if (*s2 == '?' || *s1 == *s2)
 	{
-		return (wildcmp_function(s1 + 1, s2 + 1));
+		return (1);
+	}
+	if (*s2 == '?' || (*s1 == *s2 && *s1 != '\0' && *s2 != '\0'))
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	if (*s2 == '*')
 	{
-		return (wildcmp_function(s1, s2 + 1) || wildcmp_function(s1 + 1, s2));
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	}
 	if (*s2 == '*' && *(s1 + 1) != '\0' && *s2 == '\0')
 	{
 		return (0);
 	}
 	return (0);
-}
-/**
- * wildcmp - compares two strings
- * @s1: string 1
- * @s2: string 2
- * Return: .
- */
-int wildcmp(char *s1, char *s2)
-{
-	return (wildcmp_function(s1, s2));
 }
