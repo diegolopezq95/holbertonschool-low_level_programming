9f5c1da379e2a629abd415a9f3e784ddf033f598
diegolopezq95 <927@holbertonschool.com>
2019-07-10 21:14:17 +0000

    Recursion

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -12,11 +12,11 @@ int wildcmp_function(char *s1, char *s2)
 	{
 		return (1);
 	}
-	if (*(s1 + 1) == '\0' && *s2 == '\0')
+	if (*s2 == '*' && *(s1 + 1) == '\0' && *s2 == '\0')
 	{
 		return (0);
 	}
-	if (*s2 == '?' || *s1 == *s2)
+	if (*s1 == *s2)
 	{
 		return (wildcmp_function(s1 + 1, s2 + 1));
 	}
