771e995723318c59626d119d003b159f3633d1fb
diegolopezq95 <927@holbertonschool.com>
2019-07-13 23:46:15 +0000

    argv and argc

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,20 +14,19 @@ int main(int argc, char *argv[])
 	int sum = 0;
 	int i;
 	int j;
-
-		for (i = 1; i < argc; i++)
-		{
-			for (j = 0; argv[i][j] != 0; j++)
-			{
-				if (!isdigit(argv[i][j]))
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
-			sum += atoi(argv[i]);
-		}
-		printf("%d\n", sum);
-		return (0);
-	}
+	
+	for (i = 1; i < argc; i++)
+	  {
+	    for (j = 0; argv[i][j] != 0; j++)
+	      {
+		if (!isdigit(argv[i][j]))
+		  {
+		    printf("Error\n");
+		    return (1);
+		  }
+	      }
+	    sum += atoi(argv[i]);
+	  }
+	printf("%d\n", sum);
+	return (0);
 }
