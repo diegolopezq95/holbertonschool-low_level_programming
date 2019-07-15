1675d5a827c32502a091ae3c0ffda73ef6894e56
diegolopezq95 <927@holbertonschool.com>
2019-07-15 13:52:06 +0000

    argc and argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,8 +11,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, sum = 0;
+	int i, j, sum;
 
+	sum = 0;
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
