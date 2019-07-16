83c006d1fc38e183cae65ddfbcf5c29774b13f20
diegolopezq95 <927@holbertonschool.com>
2019-07-16 18:19:04 +0000

    malloc and free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,24 +10,28 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, k;
+	int i = 0, x, y, k;
 	char *a;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (x = 0; s1[x] != '\0'; x++)
 	{
 		;
 	}
-	for (j = 0; s2[j] != '\0'; j++)
+	for (y = 0; s2[y] != '\0'; y++)
 	{
 		;
 	}
-	k = i + j + 1;
+	k = x + y + 1;
 	if (s2 == 0)
 	{
 		return (NULL);
 	}
 	a = malloc(k);
-	for (i = 0; *s1 != '\0'; i++)
+	if (a == 0)
+	{
+		return(NULL);
+	}
+	for (; *s1 != '\0'; i++)
 	{
 		a[i] = *s1;
 		s1++;
