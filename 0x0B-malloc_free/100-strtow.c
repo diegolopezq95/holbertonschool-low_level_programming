23623317b7d293c1e753d60258896ec9c0312729
diegolopezq95 <927@holbertonschool.com>
2019-07-17 21:47:00 +0000

    malloc and free 2ndch

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -3,45 +3,104 @@
 #include <stdlib.h>
 
 /**
- * **strtow - concatenates all the arg
+ * chars - count characters
  * @str: pointer string.
  * Return:.
  */
+int _characters(char *str)
+{
+	int chars = 0;
+
+	while (str[chars] != '\0')
+	{
+		if (str[chars] == ' ')
+		{
+			break;
+		}
+		chars++;
+	}
+	return (chars);
+}
+
+/**
+ * words - count words
+ * @str: pointer string.
+ * Return:.
+ */
+int _words(char *str)
+{
+	int words = 0;
+
+	while (str[chars] != '\0')
+	{
+		if (str[chars] == ' ')
+		{
+			if (str[chars + 1] == ' ' || str[chars + 1] == '\0')
+			{
+				words++;
+			}
+			chars++;
+		}
+		else
+		{
+			chars++;
+		}
+	}
+	return (words);
+}
+
+/**
+ * **strtow - splits a string into words.
+ * @str: pointer string.
+ * Return:.
+ */
+
 char **strtow(char *str)
 {
-	char *a;
-	int i, pcount, length, m = 0;
+	char **a;
+	int countchars, countwords, i = 0 ,j;
 
-	if (str == 0 || *str == '\0')
+	if (*str == 0 || str == "")
 	{
 		return (NULL);
 	}
-	i = 0;
-	length = 0;
-	while (str[length] != '\0')
-	{
-		if (str[length] == ' ')
-			i++;
-		else
-		{
-			if (str[length] == ' ' || str[length] == '\0')
-				pcount++;
-			i++;
-		}
-		length++;
-	}
-	a = malloc((length - i + pcount) * sizeof(char));
-	if (a == 0)
+	countwords = _words(str);
+	a = (char **) malloc((countwords + 1) * sizeof(char *));
+	if (a == 0 || numwords == 0 || words == 0)
 	{
 		free(a);
 		return (NULL);
 	}
-	/*for (i = 0; str[i] != '\0'; i++)*/
+	while (str[chars] != '\0')
 	{
-			if (str[i] == ' ')
-				a[m] = str[i];
-			a[m] = '\0';
-			m++;
+		if (str[chars] == ' ')
+		{
+			chars++;
+		}
+		else
+		{
+			countchars = _characters(str);
+			a[i] = (char *) malloc((countchars + 1) * sizeof(char));
+			if (a[i] == 0)
+			{
+				for (j = 0; j < i; j++)
+				{
+					free(a[j]);
+				}
+				free(a);
+				return (NULL);
+			}
+			for (i = 0; i < countchars; i++)
+			{
+				for (j = 0; a[i][j] != '\0'; j++; chars++)
+				{
+					a[i][j] = str[chars];
+					a[i][j] = '\0';
+					i++;
+				}
+				
+			}
+		}
 	}
-	return (0);
+	return (a)
 }
