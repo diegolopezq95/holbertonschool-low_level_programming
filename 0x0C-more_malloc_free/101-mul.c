eabccc5b69c300a98023bf3963026616ac336215
diegolopezq95 <927@holbertonschool.com>
2019-07-18 20:21:26 +0000

    more malloc and free

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - concatenates two strings.
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Return: .
+ */
+int main(long long int argc, char *argv[])
+{
+	long long int num1, num2, mul;
+	
+	mul = 1;
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (num1 = 1; num1 < argc; num1++)
+	{
+		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
+		{
+			if (!(argv[num1][num2] >= 48 && argv[num1][num2] <= 57))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		mul *= atoi(argv[num1]);
+	}
+	printf("%d\n", mul);
+	return (0);
+}
