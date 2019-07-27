a0134be3f9b4e33e811ab8f4659e3d7ed18c4174
diegolopezq95 <927@holbertonschool.com>
2019-07-27 17:17:56 +0000

    [fix] Variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -18,7 +18,7 @@ void print_all(const char * const format, ...)
 
 	while (format[i])
 	{
-	        switch (format[i++])
+		switch (format[i++])
 		{
 		case 'c':
 			printf("%c", va_arg(Argumentlist, int));
