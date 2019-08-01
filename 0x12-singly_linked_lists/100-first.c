3bf60eb185cc7099308193021f419ef91b95a32b
diegolopezq95 <927@holbertonschool.com>
2019-08-01 21:01:36 +0000

    Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_ - prints before main.
+ *
+ * Return: Always 0.
+ */
+void print_(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
