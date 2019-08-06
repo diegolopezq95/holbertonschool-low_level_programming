518d11093d4b3def052abc9eb9e0ff2383042c63
diegolopezq95 <927@holbertonschool.com>
2019-08-06 16:50:11 +0000

    [Fix]: More singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,7 +8,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp, *tempinsert = NULL;
+	listint_t *temp, *tempinsert;
 	unsigned int i;
 
 	if (head == NULL)
@@ -19,6 +19,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (tempinsert == NULL)
 	{
 		return (NULL);
+	}
+	if (*head == NULL)
+	{
+		tempinsert->next = *head;
+		return (tempinsert);
 	}
 	if (idx == 0)
 	{
