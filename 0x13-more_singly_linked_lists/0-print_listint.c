f8327e316cc393f3fa65dd60fc03230131dd0abc
diegolopezq95 <927@holbertonschool.com>
2019-08-06 14:06:41 +0000

    [Fix]: More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,9 +12,9 @@ size_t print_listint(const listint_t *h)
 		return (0);
 	for (number = 1; h->next != NULL; number++)
 	{
-		printf("%u\n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 	}
-	printf("%u\n", h->n);
+	printf("%d\n", h->n);
 	return (number);
 }
