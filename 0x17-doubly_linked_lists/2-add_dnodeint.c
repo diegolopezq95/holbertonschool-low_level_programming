2661983514a472996ede18bbd5ebfbb20f5691f9
diegolopezq95 <927@holbertonschool.com>
2019-09-15 21:05:49 +0000

    [Fix] add tmp varible

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,23 +7,21 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
+	dlistint_t *new, *tmp;
 
 	if (head == NULL)
 		return (NULL);
+	tmp = *head;
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	if (*head == NULL)
-	{
-		new->next = (*head);
-	}
-	else
-	{
-		new->next = *head;
-		new->prev = NULL;
-	}
 	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+	if (tmp != NULL)
+	{
+		tmp->prev = new;
+	}
 	*head = new;
 	return (new);
 }
