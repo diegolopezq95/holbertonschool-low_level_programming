169ae43c4a7420c163c6b5cc037ae7affa612c43
diegolopezq95 <927@holbertonschool.com>
2019-09-16 00:39:56 +0000

    [Fix] fixing valgrind

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -19,7 +19,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		*head = tmp->next;
 		if (tmp->next != NULL)
-			tmp->prev = NULL;
+			(*head)->prev = NULL;
 		free(tmp);
 		return (1);
 	}
