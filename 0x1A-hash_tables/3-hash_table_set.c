697e27e653d1ed9f1cb9b214db33ef259273c92c
diegolopezq95 <927@holbertonschool.com>
2019-10-11 18:39:53 +0000

    [Fix]: change all code for valgrind

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,41 +1,43 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - creates a new node.
- * @key: he key. key can not be an empty string.
- * @ht: hash table you want to add or update the key/value.
- * @value: value associated with the key.
- * Return: 1 if it succeeded, 0 otherwise.
+ * hash_table_create - creates a hash table.
+ * @ht: hash table to add or update the key/value
+ * @key: key
+ * @value: the value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new
+	hash_node_t *new, *tmp;
+	unsigned long int index = 0;
 
 	if (ht == NULL)
-		return 0;
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-		return 0;
-	new->key = strdup(key);
-	new->value = strdup(value);
-
-	add_node(ht, new);
-	return 1;
-}
-
-hash_table_t *add_node(hash_table_t *ht, hash_node_t *new)
-{
-	unsigned int key_idx;
-	hash_node_t *tmp;
-
-	key_idx = key_index(new->key, ht->size);
-	tmp = ht->new[key_idx];
-
-	while (tmp != NULL)
+		return (0);
+	if (key == NULL || key == '\0')
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = NULL;
+	tmp = ht->array[index];
+	while (tmp)
 	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (1);
+
+		}
 		tmp = tmp->next;
 	}
-
-
+	new = NULL;
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+	new->value = strdup(value);
+	new->key = strdup(key);
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
 }
