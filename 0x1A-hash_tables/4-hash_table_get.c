3e94f69e3ace2e3dbe13d767f0b74ecd872d5781
diegolopezq95 <927@holbertonschool.com>
2019-10-11 18:42:45 +0000

    Return the value associated with the element

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - retrieves a value associated with a key.
+ * @ht: hash table to look into
+ * @key: key
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *tmp;
+	unsigned long int index;
+
+	if (ht == NULL)
+		return (NULL);
+	if (key == NULL || *key == '\0')
+		return (NULL);
+	index = key_index((unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
