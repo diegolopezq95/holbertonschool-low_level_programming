b43fcc077670eb5d7e56dd6a29777e81050ec0c6
diegolopezq95 <927@holbertonschool.com>
2019-11-15 18:57:46 +0000

    [Fix]: pep8, island perimeter interview preparation

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,18 +2,19 @@
 """This module returns the perimeter of the island
 """
 
+
 def island_perimeter(grid):
     count = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 if grid[i - 1][j] == 0 and i > 0:
-                    count+= 1;
+                    count += 1
                 if grid[i + 1][j] == 0 and i < len(grid):
-                    count+= 1;
+                    count += 1
                 if grid[i][j - 1] == 0 and j > 0:
-                    count+= 1
+                    count += 1
                 if grid[i][j + 1] == 0 and j < len(grid[i]):
-                    count+= 1
+                    count += 1
     perimeter = count
     return perimeter
