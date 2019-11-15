3c52987a9bb4cba2e33e1731b5498b93a7d52bbd
diegolopezq95 <927@holbertonschool.com>
2019-11-15 19:11:53 +0000

    [Fix]: island perimeter fix range

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,17 +7,16 @@ def island_perimeter(grid):
     """
     Count perimeter method
     """
-    count = 0
+    perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if grid[i - 1][j] == 0 and i > 0:
-                    count += 1
-                if grid[i + 1][j] == 0 and i < len(grid) - 1:
-                    count += 1
-                if grid[i][j - 1] == 0 and j > 0:
-                    count += 1
-                if grid[i][j + 1] == 0 and j < len(grid[i]) - 1:
-                    count += 1
-    perimeter = count
+                if grid[i - 1][j] == 0 and i >= 0:
+                    perimeter += 1
+                if grid[i + 1][j] == 0 and i <= len(grid) - 1:
+                    perimeter += 1
+                if grid[i][j - 1] == 0 and j >= 0:
+                    perimeter += 1
+                if grid[i][j + 1] == 0 and j <= len(grid[i]) - 1:
+                    perimeter += 1
     return perimeter
