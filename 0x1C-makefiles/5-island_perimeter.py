c0904bd8e5e10d332d18ec02d2ed3c72b1cd318b
diegolopezq95 <927@holbertonschool.com>
2019-11-15 20:10:35 +0000

    [Fix]: fixing size to corner islands

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,12 +11,12 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if grid[i - 1][j] == 0 and i - 1 >= 0:
+                if i - 1 < 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-                if grid[i + 1][j] == 0 and i <= len(grid) - 1:
+                if i + 1 > len(grid) - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
-                if grid[i][j - 1] == 0 and j - 1 >= 0:
+                if j - 1 < 0 or grid[i][j - 1] == 0:
                     perimeter += 1
-                if grid[i][j + 1] == 0 and j <= len(grid[i]) - 1:
+                if j + 1 > len(grid[0]) - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
     return perimeter
