5d341eddc2f653c855be5d2685b0a9aafbe22c66
diegolopezq95 <927@holbertonschool.com>
2019-12-03 22:06:29 +0000

    [Update]: More functions

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -67,5 +67,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 /*Checks if a binary tree is full*/
 int binary_tree_is_full(const binary_tree_t *tree);
+/*Checks if a binary tree is perfect*/
+int binary_tree_is_perfect(const binary_tree_t *tree);
 
 #endif
