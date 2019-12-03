d5bcd677b8ffeed896047cfce23eee75de74e9e9
diegolopezq95 <927@holbertonschool.com>
2019-12-03 22:26:49 +0000

    [Update]: More functions

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -71,5 +71,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 /*Finds the sibling of a node*/
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+/*Finds the uncle of a node*/
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 #endif
