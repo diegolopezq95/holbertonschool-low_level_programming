35043c166458f4b6db980115ffd24130939583ed
diegolopezq95 <927@holbertonschool.com>
2019-12-13 13:57:15 +0000

    main files

---
+++ 0x1D-binary_trees/13-main.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	binary_tree_t *root;
+	size_t nodes;
+
+	root = binary_tree_node(NULL, 98);
+	root->left = binary_tree_node(root, 12);
+	root->right = binary_tree_node(root, 402);
+	binary_tree_insert_right(root->left, 54);
+	binary_tree_insert_left(root->left, 18);
+	binary_tree_insert_left(root->left, 9);
+	binary_tree_insert_right(root, 128);
+	binary_tree_print(root);
+
+	nodes = binary_tree_nodes(root);
+	printf("Nodes in %d: %lu\n", root->n, nodes);
+	nodes = binary_tree_nodes(root->right);
+	printf("Nodes in %d: %lu\n", root->right->n, nodes);
+	nodes = binary_tree_nodes(root->left->left);
+        printf("Nodes in %d: %lu\n", root->left->left->n, nodes);
+	nodes = binary_tree_nodes(root->left->right);
+	printf("Nodes in %d: %lu\n", root->left->right->n, nodes);
+	nodes = binary_tree_nodes(root->left->left->left);
+        printf("Nodes in %d: %lu\n", root->left->left->left->n, nodes);
+
+
+	return (0);
+}
