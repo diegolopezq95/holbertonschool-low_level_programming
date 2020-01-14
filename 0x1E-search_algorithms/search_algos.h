eda83ca7f91d3ac71f2fd59acfebc4fd17ca9ce4
diegolopezq95 <927@holbertonschool.com>
2020-01-14 14:56:03 +0000

    Linear search algorithm

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef _SEARCH_HEADER_
+#define _SEARCH_HEADER_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Prototype Functions */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
