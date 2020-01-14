c6d17d6396ef486277cf09fed03eddb3425dd32f
diegolopezq95 <927@holbertonschool.com>
2020-01-14 22:16:38 +0000

    Jump search algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,6 @@
 #ifndef _SEARCH_HEADER_
 #define _SEARCH_HEADER_
-
+#define MIN(a,b) (((a)<(b))?(a):(b))
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,5 +8,7 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int binary_search_recursive(int *array, size_t lo, size_t hi, int value);
+
+int jump_search(int *array, size_t size, int value);
 
 #endif
