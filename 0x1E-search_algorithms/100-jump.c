dececcec74044735980ab41a23006d706df2ee20
diegolopezq95 <927@holbertonschool.com>
2020-01-15 17:38:50 +0000

    [Fix]: change all

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -22,7 +22,7 @@ int jump_search(int *array, size_t size, int value)
 	{
 		if (array[prev] >= value)
 			break;
-		printf("Value checked array[%lu] = [%d]\n",prev, array[prev]);
+		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
 		prev += jmp;
 	}
 	i = prev - jmp;
